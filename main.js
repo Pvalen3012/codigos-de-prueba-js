abd10eacf8d9be19bff17af00f5f1422b7c6f6a3
paola <paolafig3012@gmail.com>
2023-10-04 13:51:55 -0300

    modelo de segunda preentregajs

--- main.js
+++ main.js
@@ -1,25 +1,77 @@
 
 
-    alert("¡Hola! Bienvenido.");
+    alert("¡Hola mundo!");
 
 
 
-const numeroSecreto = Math.floor(Math.random(55) * 100) + 1;
-        let intentos = 0;
 
-        const input = document.getElementById("numeroInput");
-        const boton = document.getElementById("adivinarBtn");
-        const mensaje = document.getElementById("mensaje");
+function Task(description, completed) {
+    this.description = description;
+    this.completed = completed || false;
+}
 
-        boton.addEventListener("click", function () {
-            const numeroAdivinado = parseInt(input.value);
-            intentos++;
 
-            if (numeroAdivinado === numeroSecreto) {
-                mensaje.textContent = `¡Correcto! Lo adivinaste en ${intentos} intentos.`;
-            } else if (numeroAdivinado < numeroSecreto) {
-                mensaje.textContent = "El número es mayor. Sigue intentando.";
-            } else {
-                mensaje.textContent = "El número es menor. Sigue intentando.";
-            }
-        });
+const taskList = [];
+
+
+function addTask(description) {
+    const newTask = new Task(description);
+    taskList.push(newTask);
+    console.log(`Tarea "${description}" agregada.`);
+}
+
+
+function showTaskList() {
+    console.log("Lista de Tareas:");
+    taskList.forEach((task, index) => {
+        const status = task.completed ? "[x]" : "[ ]";
+        console.log(`${index + 1}. ${status} ${task.description}`);
+    });
+}
+
+
+function completeTask(index) {
+    if (index >= 0 && index < taskList.length) {
+        taskList[index].completed = true;
+        console.log(`Tarea "${taskList[index].description}" marcada como completada.`);
+    } else {
+        console.log("Índice de tarea no válido.");
+    }
+}
+
+
+function main() {
+    console.log("Bienvenido al Simulador de Lista de Tareas!");
+
+    while (true) {
+        console.log("\nOpciones:");
+        console.log("1. Agregar tarea");
+        console.log("2. Mostrar lista de tareas");
+        console.log("3. Marcar tarea como completada");
+        console.log("4. Salir");
+
+        const choice = prompt("Ingrese el número de la opción deseada:");
+
+        switch (choice) {
+            case "1":
+                const taskDescription = prompt("Ingrese la descripción de la tarea:");
+                addTask(taskDescription);
+                break;
+            case "2":
+                showTaskList();
+                break;
+            case "3":
+                const taskIndex = prompt("Ingrese el número de la tarea que desea marcar como completada:") - 1;
+                completeTask(taskIndex);
+                break;
+            case "4":
+                console.log("Gracias por usar el Simulador de Lista de Tareas. ¡Hasta luego!");
+                return;
+            default:
+                console.log("Opción no válida. Por favor, ingrese un número válido.");
+        }
+    }
+}
+
+
+main();
